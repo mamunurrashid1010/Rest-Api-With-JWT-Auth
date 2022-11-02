84c659bd9a8bbbf3c8947de87556f6f19069056a
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-11-02 12:15:28 +0600

    logout route create

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,5 @@ Route::group(['middleware'=>'api','prefix'=>'auth'],function ($router){
     Route::post('/register',[AuthController::class,'register']);
     Route::post('/login',[AuthController::class,'login']);
     Route::get('/profile',[AuthController::class,'profile']);
+    Route::post('/logout',[AuthController::class,'logout']);
 });
